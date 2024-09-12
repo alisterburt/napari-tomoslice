bd41f336648d6248126e40f33d9f5343d8856aa4
Aysecan Unal <aysecan.uenal@h-its.org>
2024-09-12 18:44:22 +0200

    Generating output files for spheres

--- src/napari_tomoslice/pose_generation/__init__.py
+++ src/napari_tomoslice/pose_generation/__init__.py
@@ -1,4 +1,5 @@
 from .cli import pose_generation_cli
 from .points import generate_poses_points
+from .spheres import generate_poses_spheres
 from .paths import pose_generation_paths_cli
-
+from .dipoles import pose_generation_dipoles_cli
---
+++ src/napari_tomoslice/pose_generation/spheres.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+import starfile
+import typer
+import pandas as pd
+
+from .cli import pose_generation_cli
+from napari_tomoslice.console import console
+
+
+@pose_generation_cli.command(name='spheres', no_args_is_help=True)
+def generate_poses_spheres(
+    annotations_directory: Path = typer.Option(...),
+    output_star_file: Path = typer.Option(...),
+):
+    annotation_files = list(annotations_directory.glob('*_spheres.star'))
+    console.log(f'Found {len(annotation_files)} files in {annotations_directory}')
+    dfs = []
+    for file in annotation_files:
+        tilt_series_id = file.stem.rstrip('_spheres')
+        df = starfile.read(file)
+        console.log(f'Found {len(df)} particles in {file.name}')
+        df['id'] = [tilt_series_id] * len(df)
+        dfs.append(df)
+
+    df = pd.concat(dfs)
+    sorted_df = df.sort_values('id')
+    starfile.write(sorted_df, output_star_file)
+    console.log(f'Writing {len(df)} particles into {output_star_file}')
