ba1aec25b12c1f5d74ad9c2d94ac2804620e9141
Aysecan Unal <aysecan.uenal@h-its.org>
2024-09-13 13:49:45 +0200

    Generating poses from sphere annotations - Correction

--- src/napari_tomoslice/pose_generation/spheres.py
+++ src/napari_tomoslice/pose_generation/spheres.py
@@ -3,9 +3,11 @@ from pathlib import Path
 import starfile
 import typer
 import pandas as pd
+from morphosamplers import Sphere, sphere_samplers
 
 from .cli import pose_generation_cli
 from napari_tomoslice.console import console
+
 
 
 @pose_generation_cli.command(name='spheres', no_args_is_help=True)
@@ -15,15 +17,43 @@ def generate_poses_spheres(
 ):
     annotation_files = list(annotations_directory.glob('*_spheres.star'))
     console.log(f'Found {len(annotation_files)} files in {annotations_directory}')
-    dfs = []
+    all_dfs = []
     for file in annotation_files:
         tilt_series_id = file.stem.rstrip('_spheres')
         df = starfile.read(file)
         console.log(f'Found {len(df)} particles in {file.name}')
-        df['id'] = [tilt_series_id] * len(df)
-        dfs.append(df)
+        centers = df[['x', 'y', 'z']].values
+        radii = df['radius'].values
+        particle_dfs = []
 
-    df = pd.concat(dfs)
-    sorted_df = df.sort_values('id')
-    starfile.write(sorted_df, output_star_file)
-    console.log(f'Writing {len(df)} particles into {output_star_file}')
+        for center, radius in zip(centers, radii):
+            sphere = Sphere(center=center, radius=radius)
+            pose_sampler = sphere_samplers.PoseSampler(spacing=5)
+            poses = pose_sampler.sample(sphere)
+            positions = poses.positions
+            orientations = poses.orientations[:, :, 2]
+            pose_data = {
+                'x': positions[:, -1],
+                'y': positions[:, -2],
+                'z': positions[:, -3],
+                'direction_x': orientations[:, -1],
+                'direction_y': orientations[:, -2],
+                'direction_z': orientations[:, -3],
+                'id': [tilt_series_id] * len(positions)
+            }
+            particle_df = pd.DataFrame(pose_data)
+            particle_dfs.append(particle_df)
+        file_df = pd.concat(particle_dfs)
+        all_dfs.append(file_df)
+    final_df = pd.concat(all_dfs)
+    sorted_df = final_df.sort_values('id')
+    starfile.write(sorted_df, output_star_file, overwrite=True)
+    console.log(f'Writing {len(final_df)} particles into {output_star_file}')
+
+
+
+
+
+
+
+
