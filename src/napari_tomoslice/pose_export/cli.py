a47243aa961fea1f845cf5e1acfb3155336c7d09
Alister Burt <alisterburt@gmail.com>
2024-10-07 11:22:27 -0700

    add RELION (v5) pose export

--- src/napari_tomoslice/pose_export/cli.py
+++ src/napari_tomoslice/pose_export/cli.py
@@ -1,9 +1,27 @@
+from enum import Enum
+from pathlib import Path
+
+import starfile
 import typer
 
-from napari_tomoslice._constants import POSE_EXPORT_CLI_NAME
+from napari_tomoslice.console import console
+from .relion5 import export_poses_relion5
 
-pose_export_cli = typer.Typer(
-    name=POSE_EXPORT_CLI_NAME,
-    no_args_is_help=True,
-    help="export compatible metadata for a variety of different software packages",
-)
+
+class OutputType(Enum):
+    RELION5 = "relion5"
+
+
+def pose_export_cli(
+    input_file: Path = typer.Option(...),
+    output_type: OutputType = typer.Option(...),
+    output_file: Path = typer.Option(...)
+) -> None:
+    console.log(f'reading {input_file}...')
+    df = starfile.read(input_file)
+    console.log(f'read {len(df)} rows from {input_file}')
+
+    if output_type == OutputType.RELION5:
+        export_poses_relion5(df, output_file)
+    else:
+        raise NotImplementedError(output_type)
