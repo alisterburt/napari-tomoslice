8c370b1766f86bdc53459dd09cc6e64ffe9ee188
Alister Burt <alisterburt@gmail.com>
2024-11-13 10:00:23 -0800

    wrap pose generation in try/except

--- src/napari_tomoslice/pose_generation/paths.py
+++ src/napari_tomoslice/pose_generation/paths.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 import starfile
 import typer
-
 from morphosamplers import Path as MorphoPath, path_samplers
 from scipy.spatial.transform import Rotation as R
 
@@ -32,42 +32,27 @@ def generate_poses_paths_backbone(
         # read file
         df = starfile.read(file)
 
-        # isolate paths and determine tilt series identifier
+        # group control points for each path and determine tilt series identifier
         grouped_paths = df.groupby('path_id')
         tilt_series_id = file.stem.rstrip('_paths')
         console.log(f'Found {grouped_paths.ngroups} paths in {file.name}')
 
         for path_id, path_df in grouped_paths:
-            # create a morphosamplers Path object
             control_points_xyz = path_df[['x', 'y', 'z']].to_numpy()
-            path = MorphoPath(control_points=control_points_xyz)
+            try:
+                path_df = _generate_pose_df_backbone(
+                    control_points_xyz=control_points_xyz,
+                    spacing=distance_between_particles,
+                    path_id=path_id,
+                    tilt_series_id=tilt_series_id
+                )
+                path_dfs.append(_generate_pose_df_backbone())
+                console.log(f'Generated {len(path_df)} particles for path {path_id} in {tilt_series_id}')
+            except ValueError:
+                console.warning(f'Could not generate pose for path {path_id} in {tilt_series_id}')
+                console.warning(f'this is usually due to the presence of duplicate control points...')
 
-            # sample positions and orientations along the path
-            pose_sampler = path_samplers.PoseSampler(spacing=distance_between_particles)
-            poses = pose_sampler.sample(path)
 
-            # Extract positions and euler angles
-            positions = poses.positions
-            euler_angles = (
-                R.from_matrix(poses.orientations)
-                .inv()
-                .as_euler(seq='ZYZ', degrees=True)
-            )
-
-            # prepare dataframe for output
-            particle_pose_data = {
-                'x': poses.positions[:, 0],
-                'y': positions[:, 1],
-                'z': positions[:, 2],
-                'rot': euler_angles[:, 0],
-                'tilt': euler_angles[:, 1],
-                'psi': euler_angles[:, 2],
-                'manifold_id': [f"{path_id}"] * len(positions),
-                'tilt_series_id': [tilt_series_id] * len(positions)
-            }
-            path_df = pd.DataFrame(particle_pose_data)
-            path_dfs.append(path_df)
-            console.log(f'Generated {len(path_df)} particles for path {path_id} in {tilt_series_id}')
 
     # write output file to disk
     df_out = pd.concat(path_dfs)
@@ -76,13 +61,44 @@ def generate_poses_paths_backbone(
     console.log('Done!')
 
 
+def _generate_pose_df_backbone(
+    control_points_xyz: np.ndarray, spacing: float, path_id: int, tilt_series_id: str
+) -> pd.DataFrame:
+    path = MorphoPath(control_points=control_points_xyz)
+
+    # sample positions and orientations along the path
+    pose_sampler = path_samplers.PoseSampler(spacing=spacing)
+    poses = pose_sampler.sample(path)
+
+    # Extract positions and euler angles
+    positions = poses.positions
+    euler_angles = (
+        R.from_matrix(poses.orientations)
+        .inv()
+        .as_euler(seq='ZYZ', degrees=True)
+    )
+
+    # prepare dataframe for output
+    particle_pose_data = {
+        'x': poses.positions[:, 0],
+        'y': positions[:, 1],
+        'z': positions[:, 2],
+        'rot': euler_angles[:, 0],
+        'tilt': euler_angles[:, 1],
+        'psi': euler_angles[:, 2],
+        'manifold_id': [f"{path_id}"] * len(positions),
+        'tilt_series_id': [tilt_series_id] * len(positions)
+    }
+    return pd.DataFrame(particle_pose_data)
+
+
 @pose_generation_paths_cli.command(name='rings', no_args_is_help=True)
 def generate_poses_paths_rings(
-        annotations_directory: Path = typer.Option(...),
-        output_star_file: Path = typer.Option(...),
-        distance_between_particles: float = typer.Option(...),
-        number_of_points_per_ring: int = typer.Option(...),
-        ring_radius: float = typer.Option(...)
+    annotations_directory: Path = typer.Option(...),
+    output_star_file: Path = typer.Option(...),
+    distance_between_particles: float = typer.Option(...),
+    number_of_points_per_ring: int = typer.Option(...),
+    ring_radius: float = typer.Option(...)
 
 ):
     # find annotation files
