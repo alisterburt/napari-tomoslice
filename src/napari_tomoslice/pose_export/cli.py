0f351ed09173de780f7333cb1e058b4bd245a934
aysecanuenal <150450168+aysecanuenal@users.noreply.github.com>
2025-02-26 18:20:42 +0100

    add Dynamo pose export (#31)
    * add Dynamo pose export
    
    * Update src/napari_tomoslice/pose_export/dynamo.py
    
    * Update src/napari_tomoslice/pose_export/dynamo.py
    
    ---------
    
    Co-authored-by: alisterburt <alisterburt@gmail.com>

--- src/napari_tomoslice/pose_export/cli.py
+++ src/napari_tomoslice/pose_export/cli.py
@@ -6,10 +6,12 @@ import typer
 
 from napari_tomoslice.console import console
 from .relion5 import export_poses_relion5
+from .dynamo import export_poses_dynamo
 
 
 class OutputType(Enum):
     RELION5 = "relion5"
+    DYNAMO = 'dynamo'
 
 
 def pose_export_cli(
@@ -23,5 +25,7 @@ def pose_export_cli(
 
     if output_type == OutputType.RELION5:
         export_poses_relion5(df, output_file)
+    if output_type == OutputType.DYNAMO:
+        export_poses_dynamo(df, output_file)
     else:
         raise NotImplementedError(output_type)
---
+++ src/napari_tomoslice/pose_export/dynamo.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+import pandas as pd
+import dynamotable
+from scipy.spatial.transform import Rotation as R
+
+from napari_tomoslice.console import console
+
+def export_poses_dynamo(df: pd.DataFrame, output_file: Path):
+    console.log('converting star file into Dynamo compatible output')
+    eulers = df[['rot', 'tilt', 'psi']].to_numpy()
+    rotation_matrices = R.from_euler(angle=eulers, seq='ZYZ', degrees=True).inv().as_matrix()
+    eulers_dynamo = R.from_matrix(rotation_matrices).inv().as_euler(seq='zxz', degrees=True)
+    dynamo_data = {
+        'x': df['x'],
+        'y': df['y'],
+        'z': df['z'],
+        'tdrot': eulers_dynamo[:, 0],
+        'tilt': eulers_dynamo[:, 1],
+        'narot': eulers_dynamo[:, 2],
+        'tomo': df['tilt_series_id'],
+    }
+    if 'manifold_id' in df.columns:
+        dynamo_data['reg'] = df['manifold_id']
+
+    dynamo_df = pd.DataFrame(dynamo_data)
+    dynamotable.write(dynamo_df, output_file)
+
+    console.log(f'writing {len(df)} particles to {output_file}...')
+    console.log('done!')
+
+
+
+
