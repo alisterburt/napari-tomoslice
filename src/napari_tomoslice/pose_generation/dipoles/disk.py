f22fabf475ac9774ffdad15eee2abdf2104a8e8c
Aysecan Unal <aysecan.uenal@h-its.org>
2025-03-03 21:02:46 +0100

    prettify console output and add more help text for dipoles disk

--- src/napari_tomoslice/pose_generation/dipoles/disk.py
+++ src/napari_tomoslice/pose_generation/dipoles/disk.py
@@ -13,7 +13,8 @@ from napari_tomoslice.pose_generation.cli import pose_generation_cli as pose_gen
 from .cli import pose_generation_dipoles_cli as pose_generation_dipoles_cli
 
 
-@pose_generation_dipoles_cli.command(name='disk', no_args_is_help=True)
+@pose_generation_dipoles_cli.command(name='disk', no_args_is_help=True,
+                                     help='particle poses on a disk around the annotated dipoles')
 def generate_poses_dipoles_disk(
     annotations_directory: Path = typer.Option(...),
     output_star_file: Path = typer.Option(...),
@@ -51,7 +52,7 @@ def generate_poses_dipoles_disk(
     df_out = pd.concat(dipole_dfs)
     console.log(f'Writing {len(df_out)} particles into {output_star_file}...')
     starfile.write({"particles": df_out}, output_star_file, overwrite=True)
-    console.log('Done!')
+    console.log('Done!', style="bold green")
 
 def _generate_pose_df_disk(center, direction,
                            spacing:float,
