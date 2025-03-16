4b4facbe64e2f74e370d405ae5afe68866f1ef4a
Aysecan Unal <aysecan.uenal@h-its.org>
2025-03-16 21:47:41 +0100

    make sphere pose generation output generate euler angles and wrap in try/except

--- src/napari_tomoslice/pose_generation/spheres.py
+++ src/napari_tomoslice/pose_generation/spheres.py
@@ -3,7 +3,9 @@ from pathlib import Path
 import starfile
 import typer
 import pandas as pd
+
 from morphosamplers import Sphere, sphere_samplers
+from scipy.spatial.transform import Rotation as R
 
 from .cli import pose_generation_cli
 from napari_tomoslice.console import console
@@ -19,42 +21,63 @@ def generate_poses_spheres(
 ):
     annotation_files = list(annotations_directory.glob('*_spheres.star'))
     console.log(f'Found {len(annotation_files)} files in {annotations_directory}')
-    all_dfs = []
+    if len(annotation_files) == 0:
+        return
+
+    # empty list to store sphere dataframes
+    sphere_dfs = []
+
+    # iterate over files and spheres in each file
     for file in annotation_files:
-        tilt_series_id = file.stem.rstrip('_spheres')
         df = starfile.read(file)
+        tilt_series_id = file.stem.rstrip('_spheres')
         console.log(f'Found {len(df)} particles in {file.name}')
-        centers = df[['x', 'y', 'z']].values
-        radii = df['radius'].values
+        centers = df[['x', 'y', 'z']].to_numpy()
+        radii = df['radius'].to_numpy()
         particle_dfs = []
 
         for center, radius in zip(centers, radii):
-            sphere = Sphere(center=center, radius=radius)
-            pose_sampler = sphere_samplers.PoseSampler(spacing=distance_between_particles)
-            poses = pose_sampler.sample(sphere)
-            positions = poses.positions
-            orientations = poses.orientations[:, :, 2]
-            pose_data = {
-                'x': positions[:, -1],
-                'y': positions[:, -2],
-                'z': positions[:, -3],
-                'direction_x': orientations[:, -1],
-                'direction_y': orientations[:, -2],
-                'direction_z': orientations[:, -3],
-                'id': [tilt_series_id] * len(positions)
-            }
-            particle_df = pd.DataFrame(pose_data)
-            particle_dfs.append(particle_df)
+            try:
+                particle_df = _generate_pose_df_sphere(center, radius, distance_between_particles, tilt_series_id)
+                particle_dfs.append(particle_df)
+
+            except ValueError:
+                console.log(f'Could not generate pose for sphere at center {center} in {tilt_series_id}')
+
         file_df = pd.concat(particle_dfs)
-        all_dfs.append(file_df)
-    final_df = pd.concat(all_dfs)
-    sorted_df = final_df.sort_values('id')
+        sphere_dfs.append(file_df)
+
+    # write output to disk
+    final_df = pd.concat(sphere_dfs)
+    sorted_df = final_df.sort_values('tilt_series_id')
     starfile.write(sorted_df, output_star_file, overwrite=True)
     console.log(f'Writing {len(final_df)} particles into {output_star_file}')
     console.log('Done!', style="bold green")
 
+def _generate_pose_df_sphere(center, radius:float,
+                             distance_between_particles:float,
+                             tilt_series_id:str)-> pd.DataFrame:
+    # create a morphosamplers Sphere object
+    sphere = Sphere(center=center, radius=radius)
+    # sample poses on the sphere
+    pose_sampler = sphere_samplers.PoseSampler(spacing=distance_between_particles)
+    poses = pose_sampler.sample(sphere)
+    # extract positions and euler angles
+    positions = poses.positions
+    orientations = poses.orientations
+    euler_angles = R.from_matrix(orientations).inv().as_euler('ZYZ', degrees=True)
 
-
+    # construct dataframe
+    pose_data = {
+        'x': positions[:, 0],
+        'y': positions[:, 1],
+        'z': positions[:, 2],
+        'rot': euler_angles[:, 0],
+        'tilt': euler_angles[:, 1],
+        'psi': euler_angles[:, 2],
+        'tilt_series_id': [tilt_series_id] * len(positions)
+    }
+    return pd.DataFrame(pose_data)
 
 
 
